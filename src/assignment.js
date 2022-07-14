710ca5238931e09d2b4b7780db522078507d942d
Adeoluwa Falade <98970208+Kuv777@users.noreply.github.com>
2022-07-14 17:55:57 +0000

    check challenge -1 check challenge -2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+    for (let i = 0; i< arrayOfNumbers.length; i++){
+        sum += arrayOfNumbers[i];
+        console.log(sum)
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers; 
 
 /**
  * Challenge - 2
@@ -28,9 +33,14 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0
+    for (let i = 0; i < arrayOfNumbers.length; i++)
+    if(arrayOfNumbers [i] % 2 === 0){
+      count++;
+    }
+    return count
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
